67b59f2d006bd7fa9ba2ad4c384cd472038508d9
abraaovalentim <abraaotaku@gmail.com>
2024-06-25 09:55:07 -0300

    refact: change urls to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -221,3 +221,10 @@ export const defaultEEInfo: IEEInfo = {
   minScale: 0,
   maxScale: 1,
 };
+
+export const MAP_TIFF_STYLE =
+  "https://api.maptiler.com/maps/basic-v2/style.json?key=71L2QPZ0FHRofxg3QtVC";
+export const MAP_TIFF_BRAZIL_STATES =
+  "https://raw.githubusercontent.com/OCA-UFCG/countries-geo-data/main/public/data/brazil-states.geojson";
+export const MAP_TIFF_BRAZIL_CITIES =
+  "https://raw.githubusercontent.com/OCA-UFCG/municipal-brazilian-geodata/master/minified/cities.min.json";
