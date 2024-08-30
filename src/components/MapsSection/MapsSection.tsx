6bd7fe5fbb9cc52015d669e44271f001954ec13f
abraaovalentim <abraaotaku@gmail.com>
2024-08-30 17:10:55 -0300

    refact/refact the maps section

--- src/components/MapsSection/MapsSection.tsx
+++ src/components/MapsSection/MapsSection.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { LinkButton, SectionTitle, SpinningIcon } from "@/app/globalStyles";
+import { SectionTitle, SpinningIcon } from "@/app/globalStyles";
 import {
   Description,
   LoadingContainer,
   MapPoster,
-  Tag,
+  ExpandBox,
+  TagButton,
   TagsContainer,
+  VisuHeader,
+  VisuIcon,
+  IconWrapper,
+  Divider,
+  LinkButton,
   VisuName,
-  Wrapper,
+  MapSectionWrapper,
+  BoxWrapper,
+  PreviewWrapper,
 } from "./MapsSection.styles";
 import { useContext, useEffect, useState } from "react";
 import { IEEInfo } from "@/utils/interfaces";
@@ -21,8 +29,13 @@ const MapsSection = () => {
   const [currentVisu, setCurrentVisu] = useState<IEEInfo>(defaultEEInfo);
   const [loading, setLoading] = useState<boolean>(true);
   const { mapsData } = useContext(CMSContext);
+  let handler: NodeJS.Timeout;
 
-  const updateCurrentVisu = (visuId: string) => {
+  const updateCurrentVisu = (
+    visuId: string,
+    event?: { preventDefault: () => void } | undefined,
+  ) => {
+    event?.preventDefault();
     if (visuId !== currentVisu.id) {
       setCurrentVisu(
         mapsData.find((map) => map.fields.id === visuId)?.fields ??
@@ -30,6 +43,30 @@ const MapsSection = () => {
       );
     }
   };
+
+  const nextVisu = () => {
+    const currentIndex = mapsData.findIndex(
+      (map) => map.fields.id === currentVisu.id,
+    );
+    const nextIndex = (currentIndex + 1) % mapsData.length;
+    updateCurrentVisu(mapsData[nextIndex].fields.id);
+  };
+
+  const visuDebounce = () => {
+    handler = setTimeout(() => {
+      nextVisu();
+    }, 7000);
+  };
+
+  useEffect(() => {
+    if (mapsData.length > 1) {
+      visuDebounce();
+    }
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [currentVisu, mapsData]);
 
   useEffect(() => {
     if (mapsData.length != 0) {
@@ -40,43 +77,70 @@ const MapsSection = () => {
   }, [mapsData]);
 
   return (
-    <Wrapper id="maps-visu">
-      <SectionTitle variation="white">Mapas e Visualizações</SectionTitle>
-      <TagsContainer>
-        {mapsData.map(({ fields: tag }: { fields: IEEInfo }) => (
-          <Tag
-            key={tag.id}
-            active={(tag.id === currentVisu.id).toString()}
-            onClick={() => updateCurrentVisu(tag.id)}
-          >
-            {capitalize(tag.name)}
-          </Tag>
-        ))}
-      </TagsContainer>
-      {loading ? (
-        <LoadingContainer>
-          <SpinningIcon
-            src={LoadingImage}
-            alt="Representação de uma flor que existe no nordete com cinco pétalas"
-          />
-          <span>Carregando..</span>
-        </LoadingContainer>
-      ) : (
-        <MapPoster
-          alt=""
-          width={500}
-          height={400}
-          src={
-            typeof currentVisu.poster === "object"
-              ? `https:${currentVisu.poster.fields.file.url}`
-              : currentVisu.poster
-          }
-        />
-      )}
-      <VisuName>{capitalize(currentVisu.name)}</VisuName>
-      <Description>{currentVisu.description}</Description>
-      <LinkButton href={`/map?name=${currentVisu.id}`}>Visualizar</LinkButton>
-    </Wrapper>
+    <MapSectionWrapper id="maps-visu">
+      <SectionTitle variation="black">Mapas e Visualizações</SectionTitle>
+      <BoxWrapper>
+        {loading ? (
+          <LoadingContainer>
+            <SpinningIcon
+              src={LoadingImage}
+              alt="Representação de uma flor que existe no nordete com cinco pétalas"
+            />
+            <span>Carregando..</span>
+          </LoadingContainer>
+        ) : (
+          <PreviewWrapper>
+            <ExpandBox href={`/map?name=${currentVisu.id}`}>
+              <VisuIcon id="expand" size={20} />
+            </ExpandBox>
+            <MapPoster
+              alt=""
+              width={500}
+              height={400}
+              src={
+                typeof currentVisu.poster === "object"
+                  ? `https:${currentVisu.poster.fields.file.url}`
+                  : currentVisu.poster
+              }
+            />
+          </PreviewWrapper>
+        )}
+        {mapsData.length != 0 && (
+          <TagsContainer>
+            {mapsData.map(({ fields: tag }: { fields: IEEInfo }) => (
+              <TagButton
+                key={tag.id}
+                active={(tag.id === currentVisu.id).toString()}
+                onClick={(e) => updateCurrentVisu(tag.id, e)}
+              >
+                <VisuHeader>
+                  <VisuName active={(tag.id === currentVisu.id).toString()}>
+                    {capitalize(tag.name)}
+                  </VisuName>
+                  <IconWrapper>
+                    <VisuIcon
+                      active={(tag.id === currentVisu.id).toString()}
+                      id={tag.id === currentVisu.id ? "open-eye" : "closed-eye"}
+                      size={20}
+                    />
+                    <Divider />
+                    <LinkButton
+                      href={`/map?name=${currentVisu.id}`}
+                      active={(tag.id === currentVisu.id).toString()}
+                    >
+                      Visualizar
+                    </LinkButton>
+                  </IconWrapper>
+                </VisuHeader>
+                <Description active={(tag.id === currentVisu.id).toString()}>
+                  {currentVisu.description}
+                </Description>
+              </TagButton>
+            ))}
+          </TagsContainer>
+        )}
+      </BoxWrapper>
+    </MapSectionWrapper>
   );
 };
 
