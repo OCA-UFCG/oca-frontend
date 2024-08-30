4508b73f8521b7fc00e9291aea38878f51b0183b
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-08-29 22:22:58 -0300

    Chore: Update ssr on home section

---
+++ src/components/MapTiff/Section/MapSection.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { Suspense, useCallback, useEffect, useState } from "react";
+import { IEEInfo, IMapInfo } from "@/utils/interfaces";
+import MapTiff from "@/components/MapTiff/MapTiff";
+import MapTemplate from "@/templates/mapTemplate";
+import MapsMenu from "@/components/MapsMenu/MapsMenu";
+import { defaultEEInfo } from "@/utils/constants";
+import {
+  HeaderWrapper,
+  HomeImage,
+  MapContainer,
+  MapLegendContainer,
+  MenuWrapper,
+  NameContainer,
+  QuestionImage,
+  QuestionWrapper,
+  VisuName,
+} from "./MapsSections.styles";
+import Link from "next/link";
+import { capitalize } from "@/utils/functions";
+import MapDescription from "@/components/MapDescription/MapDescription";
+import { MapLegend } from "@/components/MapLegend/MapLegend";
+
+const MapPageWrapper = ({ mapsData }: { mapsData: any }) => (
+  <Suspense fallback={<div>Carregando...</div>}>
+    <MapSection mapsData={mapsData} />
+  </Suspense>
+);
+
+const DEFAULT_TIFF = "spei";
+
+const MapSection = ({ mapsData }: { mapsData: any }) => {
+  const searchParams = useSearchParams();
+
+  const [loadingMap, setLoadingMap] = useState<boolean>(false);
+  const [imageData, setImageData] = useState<IMapInfo>({
+    name: "",
+    year: "",
+  });
+
+  const [descriptionInfo, setDescriptionInfo] =
+    useState<IEEInfo>(defaultEEInfo);
+  const [isDescRetracted, setIsDescRetracted] = useState<boolean>(true);
+
+  const handleDescUpdate = useCallback(
+    (name: string, retract: boolean = true) => {
+      if (retract) {
+        if (name === descriptionInfo.id) {
+          setIsDescRetracted(!isDescRetracted);
+        } else {
+          setIsDescRetracted(false);
+        }
+      }
+      setDescriptionInfo(
+        mapsData.filter(
+          (data: { fields: { id: string } }) => data.fields.id === name,
+        )[0].fields,
+      );
+    },
+    [descriptionInfo.id, isDescRetracted, mapsData],
+  );
+
+  const [isMenuRetracted, setIsmenuRetracted] = useState<boolean>(false);
+
+  const handleMapClick = useCallback(() => {
+    setIsmenuRetracted(true);
+    setIsDescRetracted(true);
+  }, [setIsmenuRetracted, setIsDescRetracted]);
+
+  useEffect(() => {
+    if (mapsData.length === 0) return;
+
+    let name = searchParams?.get("name") ?? "spei";
+    let year = searchParams?.get("year") ?? "general";
+
+    const filteredData = mapsData.filter(
+      (data: { fields: { id: string } }) => data.fields.id === name,
+    );
+    if (filteredData.length === 0) {
+      name = DEFAULT_TIFF;
+    }
+
+    if (
+      mapsData &&
+      mapsData.filter(
+        (data: { fields: { id: string; imageData: {} } }) =>
+          name === data.fields.id && year in Object.keys(data.fields.imageData),
+      ).length == 0
+    ) {
+      year = Object.keys(filteredData[0].fields.imageData)[0];
+    }
+
+    setImageData({ name, year });
+    setDescriptionInfo(filteredData[0].fields);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mapsData]);
+
+  return (
+    <MapTemplate>
+      <MapContainer>
+        <MapTiff
+          data={imageData}
+          onClick={handleMapClick}
+          loading={loadingMap}
+          setLoading={setLoadingMap}
+        />
+      </MapContainer>
+      <MapDescription
+        imageInfo={descriptionInfo}
+        retracted={isDescRetracted}
+        setRetracted={setIsDescRetracted}
+      />
+      <HeaderWrapper>
+        <MenuWrapper>
+          <MapsMenu
+            isLoading={loadingMap}
+            initialValues={imageData}
+            retracted={isMenuRetracted}
+            setRetracted={setIsmenuRetracted}
+            updateVisu={setImageData}
+            onQuestionSelect={handleDescUpdate}
+          />
+          <Link href="/">
+            <HomeImage id="home" size={16} />
+          </Link>
+        </MenuWrapper>
+        {imageData.name && (
+          <NameContainer>
+            <VisuName>
+              {capitalize(
+                mapsData.filter(
+                  (data: { fields: { id: string } }) =>
+                    data.fields.id === imageData.name,
+                )[0].fields.name,
+              )}
+            </VisuName>
+            <QuestionWrapper
+              onClick={() => handleDescUpdate(imageData.name)}
+              title={`Sobre ${mapsData.filter((data: { fields: { id: string } }) => data.fields.id === imageData.name)[0].fields.name}`}
+            >
+              <QuestionImage id="question" height={20} width={20} />
+            </QuestionWrapper>
+          </NameContainer>
+        )}
+      </HeaderWrapper>
+      <MapLegendContainer>
+        {imageData.name && (
+          <MapLegend
+            imageInfo={
+              mapsData.filter(
+                (data: { fields: { id: string } }) =>
+                  data.fields.id === imageData.name,
+              )[0].fields
+            }
+            year={imageData.year || "general"}
+          />
+        )}
+      </MapLegendContainer>
+    </MapTemplate>
+  );
+};
+
+export default MapPageWrapper;
--- src/components/Publication/Section/Publications.tsx
+++ src/components/Publication/Section/Publications.tsx
@@ -3,20 +3,21 @@
 import { Section, SectionTitle } from "@/app/globalStyles";
 import { PublicationsContainer } from "./Publications.styles";
 import Publication from "../Publication";
-import { useContext, useEffect, useState } from "react";
-import { CMSContext } from "@/contexts/ContentProvider";
-import { Entry, EntrySkeletonType } from "contentful";
 import { IPublication } from "@/utils/interfaces";
 
-const PublicationsSection = () => {
-  const [publications, setPublications] = useState<
-    Entry<EntrySkeletonType, undefined, string>[]
-  >([]);
-  const { loadData } = useContext(CMSContext);
+const PublicationsSection = ({
+  publications,
+}: {
+  publications: { fields: IPublication }[];
+}) => {
+  // const [publications, setPublications] = useState<
+  //   Entry<EntrySkeletonType, undefined, string>[]
+  // >([]);
+  // const { loadData } = useContext(CMSContext);
 
-  useEffect(() => {
-    loadData("publications", setPublications);
-  }, [loadData]);
+  // useEffect(() => {
+  //   loadData("publications", setPublications);
+  // }, [loadData]);
 
   return (
     <Section full={"false"} id="publications">
