fa06323357073c66d105c8ca15fdf1b904af401e
cilasmarques <cilas@copin.ufcg.edu.br>
2024-10-03 17:15:45 -0300

    refact: refact mapTiff code

--- src/components/MapTiff/MapTiff.tsx
+++ src/components/MapTiff/MapTiff.tsx
@@ -3,7 +3,7 @@
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { createRoot } from "react-dom/client";
-import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import {
   MapContainer,
   Loading,
@@ -18,10 +18,237 @@ import {
   Color,
 } from "./MapTiff.styles";
 import { IMapInfo } from "@/utils/interfaces";
-import { MAP_TIFF_STYLE, MAP_TIFF_BRAZIL_CITIES } from "@/utils/constants";
+import {
+  MAP_TIFF_STYLE,
+  MAP_TIFF_BRAZIL_STATES,
+  MAP_TIFF_BRAZIL_CITIES,
+} from "@/utils/constants";
 import { CMSContext } from "@/contexts/ContentProvider";
 
 const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
+
+const initializeMap = (mapContainer: React.RefObject<HTMLDivElement>) => {
+  const newMap = new maplibregl.Map({
+    container: mapContainer.current!,
+    style: MAP_TIFF_STYLE,
+    center: [-55, -15],
+    zoom: 3.6,
+    maxZoom: 10,
+    minZoom: 3.6,
+  });
+
+  newMap.on("load", () => {
+    newMap.addControl(new maplibregl.NavigationControl(), "bottom-left");
+
+    newMap.addSource("brazil-states", {
+      type: "geojson",
+      data: MAP_TIFF_BRAZIL_STATES,
+    });
+
+    newMap.addLayer({
+      id: "state-fills",
+      type: "fill",
+      source: "brazil-states",
+      layout: {},
+      paint: {
+        "fill-color": "black",
+        "fill-opacity": [
+          "case",
+          ["boolean", ["feature-state", "hover"], false],
+          0.3,
+          0,
+        ],
+      },
+    });
+
+    newMap.addLayer({
+      id: "state-borders",
+      type: "line",
+      source: "brazil-states",
+      layout: {},
+      paint: {
+        "line-color": "#2D2D2D",
+        "line-width": [
+          "case",
+          ["boolean", ["feature-state", "hover"], false],
+          2.5,
+          1.5,
+        ],
+      },
+    });
+
+    newMap.addSource("brazil-cities", {
+      type: "geojson",
+      data: MAP_TIFF_BRAZIL_CITIES,
+    });
+
+    newMap.addLayer({
+      id: "brazil-cities",
+      type: "line",
+      source: "brazil-cities",
+      layout: {},
+      paint: {
+        "line-color": "#00000050",
+        "line-width": 2,
+      },
+      minzoom: 6,
+    });
+  });
+
+  return newMap;
+};
+
+const loadMapLayer = async (
+  map: maplibregl.Map | null,
+  mapsData: any[],
+  name: string,
+  year: string,
+  setLoading: (loading: boolean) => void,
+) => {
+  setLoading(true);
+  if (!map?.getSource(name + year) && mapsData.length > 0) {
+    // Filter the contentful map data by name
+    const mapData = mapsData.find((data) => data.fields.id === name)?.fields;
+    const body = JSON.stringify(mapData);
+
+    // Fetch the raster layer from the ee API
+    const response = await fetch(
+      `${HOST_URL}/api/ee?name=${name}&year=${year}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body,
+      },
+    );
+
+    if (response.status !== 200) {
+      setLoading(false);
+
+      return;
+    }
+
+    const { url } = await response.json();
+    if (!map?.getSource(name + year)) {
+      map?.addSource(name + year, {
+        type: "raster",
+        tiles: [url],
+        tileSize: 256,
+      });
+    }
+  }
+
+  // This code adds a raster layer on top of the first "symbol" layer in the map's style.
+  // It checks if the map and its layers exist, finds the first layer of type "symbol",
+  // and inserts the raster layer right above it.
+  const symbolLayer = map
+    ?.getStyle()
+    ?.layers?.find((layer) => layer.type === "symbol");
+  if (map && symbolLayer) {
+    map.addLayer(
+      {
+        type: "raster",
+        source: `${name}${year}`,
+        id: `${name}${year}`,
+      },
+      symbolLayer.id,
+    );
+  }
+  setLoading(false);
+};
+
+const addPopupEffect = (
+  map: maplibregl.Map | null,
+  mapsData: any[],
+  name: string,
+  year: string,
+  popupRef: React.MutableRefObject<maplibregl.Popup>,
+  createPopupContent: (
+    properties: any,
+    colors: string[],
+    atualData: any,
+  ) => React.ReactNode,
+) => {
+  let hoveredStateId: string | number | undefined = undefined;
+  const popupContainer = document.createElement("div");
+  const root = createRoot(popupContainer);
+
+  map?.on("mousemove", "state-fills", (e) => {
+    if (e.features && e.features.length > 0) {
+      if (hoveredStateId) {
+        map.setFeatureState(
+          { source: "brazil-states", id: hoveredStateId },
+          { hover: false },
+        );
+      }
+      hoveredStateId = e.features[0].id;
+      map.setFeatureState(
+        { source: "brazil-states", id: hoveredStateId },
+        { hover: true },
+      );
+
+      const properties = e.features[0].properties;
+      if (!properties[name + year]) popupRef.current.remove();
+      else {
+        const propertiesTIFF = JSON.parse(properties[name + year]);
+        const atualData = mapsData.filter((data) => data.fields.id === name)[0]
+          .fields;
+        const colors = atualData.imageData[year]?.imageParams.map(
+          (param: any) => param.color,
+        );
+
+        root.render(createPopupContent(propertiesTIFF, colors, atualData));
+
+        popupRef.current
+          .setLngLat(e.lngLat)
+          .setDOMContent(popupContainer)
+          .addTo(map);
+      }
+    }
+  });
+
+  map?.on("mouseleave", "state-fills", () => {
+    if (hoveredStateId) {
+      map.setFeatureState(
+        { source: "brazil-states", id: hoveredStateId },
+        { hover: false },
+      );
+    }
+    hoveredStateId = undefined;
+    popupRef.current.remove();
+  });
+};
+
+const createPopupContent = (
+  propertiesTIFF: any,
+  colors: string[],
+  atualData: any,
+) => {
+  const areas = colors
+    .map((color, index) => ({
+      color,
+      area: propertiesTIFF[`Area_km2_${index + 1}`],
+      percent: propertiesTIFF[`Percent_${index + 1}`],
+    }))
+    .filter(({ area }) => area && area > 0);
+
+  return (
+    <PopupContent>
+      <TopHeader>
+        <Title>{atualData.name}</Title>
+        <Subtitle>{atualData.subtitle}</Subtitle>
+      </TopHeader>
+      {areas.map((areaInfo, index) => (
+        <LineInfo key={index}>
+          <TotalArea>{areaInfo.area?.toFixed(0)}Km²</TotalArea>
+          <Color color={areaInfo.color} />
+          <PercentArea>{areaInfo.percent?.toFixed(0)}%</PercentArea>
+        </LineInfo>
+      ))}
+    </PopupContent>
+  );
+};
 
 const MapTiff = ({
   data,
@@ -43,255 +270,22 @@ const MapTiff = ({
     new maplibregl.Popup({ closeButton: false, closeOnClick: false }),
   );
 
-  const loadMap = useCallback(() => {
-    if (mapContainer.current) {
-      const newMap = new maplibregl.Map({
-        container: mapContainer.current,
-        style: MAP_TIFF_STYLE,
-        center: [-55, -15],
-        zoom: 3.6,
-        maxZoom: 10,
-        minZoom: 3.6,
-      });
-
-      newMap.on("load", () => {
-        newMap.addControl(new maplibregl.NavigationControl(), "bottom-left");
-
-        // ==== Add Brazil states =====
-        newMap.addSource("brazil-states", {
-          type: "geojson",
-          data: "/fc_final.geojson",
-        });
-
-        newMap.addLayer({
-          id: "state-fills",
-          type: "fill",
-          source: "brazil-states",
-          layout: {},
-          paint: {
-            "fill-color": "black",
-            "fill-opacity": [
-              "case",
-              ["boolean", ["feature-state", "hover"], false],
-              0.3,
-              0,
-            ],
-          },
-        });
-
-        // ==== Add Brazil states borders =====
-        newMap.addLayer({
-          id: "state-borders",
-          type: "line",
-          source: "brazil-states",
-          layout: {},
-          paint: {
-            "line-color": "#2D2D2D",
-            "line-width": [
-              "case",
-              ["boolean", ["feature-state", "hover"], false],
-              2.5,
-              1.5,
-            ],
-          },
-        });
-
-        // ==== Add Brazil cities =====
-        newMap.addSource("brazil-cities", {
-          type: "geojson",
-          data: MAP_TIFF_BRAZIL_CITIES,
-        });
-
-        newMap.addLayer({
-          id: "brazil-cities",
-          type: "line",
-          source: "brazil-cities",
-          layout: {},
-          paint: {
-            "line-color": "#00000050",
-            "line-width": 2,
-          },
-          minzoom: 6,
-        });
-      });
-      setMap(newMap);
-    }
-  }, []);
-
-  const loadLayer = useCallback(
-    async (name: string, year: string) => {
-      setLoading(true);
-      if (!map?.getSource(name + year) && mapsData.length > 0) {
-        const body = JSON.stringify(
-          mapsData.filter((data) => data.fields.id === name)[0].fields,
-        );
-
-        const response = await fetch(
-          `${HOST_URL}/api/ee?name=${name}&year=${year}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body,
-          },
-        );
-
-        if (response.status !== 200) {
-          setLoading(false);
-        } else {
-          const { url } = await response.json();
-          if (!map?.getSource(name + year)) {
-            map?.addSource(name + year, {
-              type: "raster",
-              tiles: [url],
-              tileSize: 256,
-            });
-          }
-        }
-      }
-
-      let firstSymbolId;
-      if (map && map.getStyle()) {
-        const layers = map?.getStyle().layers || [];
-        for (let i = 0; i < layers.length; i++) {
-          if (layers[i].type === "symbol") {
-            firstSymbolId = layers[i].id;
-            break;
-          }
-        }
-
-        map?.addLayer(
-          {
-            type: "raster",
-            source: name + year,
-            id: name + year,
-          },
-          firstSymbolId,
-        );
-        setLoading(false);
-      }
-    },
-    [map, mapsData, setLoading],
-  );
-
-  const loadFeatureCollection = useCallback(
-    (name: string, year: string) => {
-      // ==== Add hover effect on Brazil states =====
-      let hoveredStateId: string | number | undefined = undefined;
-      const popupContainer = document.createElement("div");
-      const root = createRoot(popupContainer);
-
-      map?.on("mousemove", "state-fills", (e) => {
-        if (e.features && e.features.length > 0) {
-          if (hoveredStateId) {
-            map?.setFeatureState(
-              { source: "brazil-states", id: hoveredStateId },
-              { hover: false },
-            );
-          }
-          hoveredStateId = e.features[0].id;
-          map?.setFeatureState(
-            { source: "brazil-states", id: hoveredStateId },
-            { hover: true },
-          );
-
-          const properties = e.features[0].properties;
-          if (!properties[name + year]) popupRef.current.remove();
-          else {
-            const propertiesTIFF = JSON.parse(properties[name + year]);
-
-            const atualData = mapsData.filter(
-              (data) => data.fields.id === name,
-            )[0].fields;
-
-            const colors = atualData.imageData[year]?.imageParams.map(
-              (param) => param.color,
-            );
-
-            const areas = [
-              {
-                color: colors[0],
-                area: propertiesTIFF.Area_km2_1,
-                percent: propertiesTIFF.Percent_1,
-              },
-              {
-                color: colors[1],
-                area: propertiesTIFF.Area_km2_2,
-                percent: propertiesTIFF.Percent_2,
-              },
-              {
-                color: colors[2],
-                area: propertiesTIFF.Area_km2_3,
-                percent: propertiesTIFF.Percent_3,
-              },
-              {
-                color: colors[3],
-                area: propertiesTIFF.Area_km2_4,
-                percent: propertiesTIFF.Percent_4,
-              },
-              {
-                color: colors[4],
-                area: propertiesTIFF.Area_km2_5,
-                percent: propertiesTIFF.Percent_5,
-              },
-              {
-                color: colors[5],
-                area: propertiesTIFF.Area_km2_6,
-                percent: propertiesTIFF.Percent_6,
-              },
-            ];
-
-            root.render(
-              <PopupContent>
-                <TopHeader>
-                  <Title>{properties.NM_UF}</Title>
-                  <Subtitle>{atualData.name}</Subtitle>
-                </TopHeader>
-                {areas
-                  .filter(({ area }) => area && area > 0) // Filtra os valores que têm percent maior que 0
-                  .map((areaInfo, index) => (
-                    <LineInfo key={index}>
-                      <TotalArea>{areaInfo.area?.toFixed(0)}Km²</TotalArea>
-                      <Color color={areaInfo.color} />
-                      <PercentArea>{areaInfo.percent?.toFixed(0)}%</PercentArea>
-                    </LineInfo>
-                  ))}
-              </PopupContent>,
-            );
-
-            popupRef.current
-              .setLngLat(e.lngLat)
-              .setDOMContent(popupContainer)
-              .addTo(map);
-          }
-        }
-      });
-
-      map?.on("mouseleave", "state-fills", () => {
-        if (hoveredStateId) {
-          map?.setFeatureState(
-            { source: "brazil-states", id: hoveredStateId },
-            { hover: false },
-          );
-        }
-        hoveredStateId = undefined;
-        popupRef.current.remove();
-      });
-    },
-    [map, mapsData],
-  );
-
   useEffect(() => {
+    if (name && !map) {
+      const initializedMap = initializeMap(mapContainer);
+      setMap(initializedMap);
+    }
     if (name) {
       const yearStr = year || "general";
-
-      if (!map) {
-        loadMap();
-      }
-
-      loadLayer(name, yearStr);
-      loadFeatureCollection(name, yearStr);
+      loadMapLayer(map, mapsData, name, yearStr, setLoading);
+      addPopupEffect(
+        map,
+        mapsData,
+        name,
+        yearStr,
+        popupRef,
+        createPopupContent,
+      );
 
       return () => {
         if (map?.getLayer(name + yearStr)) {
@@ -299,7 +293,7 @@ const MapTiff = ({
         }
       };
     }
-  }, [name, year, map, loadLayer, loadMap, loadFeatureCollection]);
+  }, [name, year, map, mapsData, setLoading]);
 
   return (
     <MapContainer
