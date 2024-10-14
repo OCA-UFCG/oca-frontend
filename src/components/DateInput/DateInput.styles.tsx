cb42a20010c99508e1caf869f610162c76729810
cilasmarques <cilas@copin.ufcg.edu.br>
2024-10-14 00:15:23 -0300

    refact: update MapsMenu temporal bar

--- src/components/DateInput/DateInput.styles.tsx
+++ src/components/DateInput/DateInput.styles.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { Icon } from "../Icon/Icon";
 
 export const Wrapper = styled.div<{ disabled: string }>`
   display: flex;
   gap: 0.5rem;
-  align-items: center;
   width: 100%;
   margin-top: 0.5rem;
+  align-items: center;
+  justify-content: space-around;
   ${({ disabled }) =>
     disabled === "true" &&
     `
@@ -17,18 +17,18 @@ export const Wrapper = styled.div<{ disabled: string }>`
   box-sizing: border-box;
 `;
 
-export const CalendarImage = styled(Icon)`
-  height: fit-content;
-`;
-
-export const DateInfo = styled.p`
-  width: max-content;
-  text-wrap: nowrap;
-  margin: 0;
-`;
-
 export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0rem;
   width: 100%;
+  // height: 2rem;
+`;
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 2rem;
 `;
 
 export const RangeInput = styled.input`
@@ -36,89 +36,43 @@ export const RangeInput = styled.input`
   -webkit-appearance: none;
   background: transparent;
   cursor: pointer;
-  /* z-index: 0; */
-  padding-inline: 0.25rem;
 
   input[type="range"] {
     height: 22px;
     -webkit-appearance: none;
-    margin: 10px 0;
     width: 100%;
   }
+
   &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 8px;
-    cursor: pointer;
-    box-shadow: 0px 0px 0px ${({ theme }) => theme.colors.black};
-    background: ${({ theme }) => theme.colors.black}70;
-    border-radius: 48px;
-    border: 0px solid ${({ theme }) => theme.colors.black};
-  }
-  &::-webkit-slider-thumb {
-    box-shadow: 0px 0px 2px ${({ theme }) => theme.colors.black};
-    border: 0px solid #b3b5e3;
-    height: 16px;
-    width: 16px;
-    border-radius: 16px;
-    background: ${({ theme }) => theme.colors.orange};
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -4px;
-  }
-  &:focus::-webkit-slider-runnable-track {
-    background: ${({ theme }) => theme.colors.black};
-  }
-  &::-moz-range-track {
-    width: 100%;
-    height: 8px;
-    cursor: pointer;
-    box-shadow: 0px 0px 0px ${({ theme }) => theme.colors.black};
-    background: ${({ theme }) => theme.colors.black};
-    border-radius: 48px;
-    border: 0px solid ${({ theme }) => theme.colors.black};
-  }
-  &::-moz-range-thumb {
-    box-shadow: 0px 0px 2px ${({ theme }) => theme.colors.black};
-    border: 0px solid #b3b5e3;
-    height: 16px;
-    width: 16px;
-    border-radius: 16px;
-    background: ${({ theme }) => theme.colors.orange};
-    cursor: pointer;
-  }
-  &::-ms-track {
     width: 100%;
     height: 4px;
     cursor: pointer;
-    background: transparent;
-    border-color: transparent;
-    color: transparent;
+    background: ${({ theme }) => theme.colors.black}70;
+    border-radius: 48px;
   }
-  &::-ms-fill-lower {
-    background: ${({ theme }) => theme.colors.black};
-    border: 0px solid ${({ theme }) => theme.colors.black};
-    border-radius: 96px;
-    box-shadow: 0px 0px 0px ${({ theme }) => theme.colors.black};
-  }
-  &::-ms-fill-upper {
-    background: ${({ theme }) => theme.colors.black};
-    border: 0px solid ${({ theme }) => theme.colors.black};
-    border-radius: 96px;
-    box-shadow: 0px 0px 0px ${({ theme }) => theme.colors.black};
-  }
-  &::-ms-thumb {
-    margin-top: 1px;
-    box-shadow: 0px 0px 2px ${({ theme }) => theme.colors.black};
-    height: 16px;
-    width: 16px;
+
+  &::-webkit-slider-thumb {
+    height: 12px;
+    width: 12px;
     border-radius: 16px;
+    background: ${({ theme }) => theme.colors.orange};
     cursor: pointer;
+    margin-top: -4px;
   }
-  &:focus::-ms-fill-lower {
+
+  &::-moz-range-track {
+    width: 100%;
+    height: 4px;
     background: ${({ theme }) => theme.colors.black};
+    border-radius: 48px;
   }
-  &:focus::-ms-fill-upper {
-    background: ${({ theme }) => theme.colors.black};
+
+  &::-moz-range-thumb {
+    height: 12px;
+    width: 12px;
+    border-radius: 16px;
+    background: ${({ theme }) => theme.colors.orange};
+    cursor: pointer;
   }
 
   &:disabled {
@@ -126,24 +80,29 @@ export const RangeInput = styled.input`
   }
 `;
 
-export const DateList = styled.ul`
+export const DateContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+
   position: relative;
-  transform: translate(-10px, 3px);
+  // transform: translate(0, -10px); /* Aproximar a data da barra */
   opacity: 0.7;
   z-index: -2;
 `;
 
-export const DateItem = styled.li`
+export const DateSpan = styled.span<{ year: string }>`
   position: relative;
-  list-style: none;
-  width: 2px;
-  height: 8px;
-  border-radius: 2px;
-  margin: 0 7px;
-  padding: 0;
-  background-color: ${({ theme }) => theme.colors.black};
+  display: inline-block;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.black};
   pointer-events: none;
+
+  &::after {
+    content: "${({ year }) => year}";
+    top: 8px;
+    left: 50%;
+    transform: translateX(-50%);
+    pointer-events: none;
+  }
 `;
