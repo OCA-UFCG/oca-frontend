e014740cfdb7a152a8ce45ac16f7e5497c06b651
cilasmarques <cilas@copin.ufcg.edu.br>
2024-10-18 16:48:05 -0300

    refact: remove context provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,14 +4,13 @@ import StyledComponentsRegistry from "@/lib/registry";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./globalStyles";
 import { theme } from "./theme";
-import { ContentProvider } from "@/contexts/ContentProvider";
 
 export const Providers = (props: React.PropsWithChildren) => {
   return (
     <StyledComponentsRegistry>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <ContentProvider>{props.children}</ContentProvider>
+        {props.children}
       </ThemeProvider>
     </StyledComponentsRegistry>
   );
