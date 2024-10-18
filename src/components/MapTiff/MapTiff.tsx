5ea7a11949d78670dc59008e1f713340171d868e
LazaroQueiroz <lazaro.queiroz.nascimento@ccc.ufcg.edu.br>
2024-10-18 16:54:43 -0300

    fix: fixed layers superposition (map page and maps and visu sections)

--- src/components/MapTiff/MapTiff.tsx
+++ src/components/MapTiff/MapTiff.tsx
@@ -162,7 +162,7 @@ const MapTiff = ({
           {
             type: "raster",
             source: `${name}${year}`,
-            id: `${name}${year}`,
+            id: `@oca/${name}${year}`,
           },
           symbolLayer.id,
         );
@@ -235,32 +235,42 @@ const MapTiff = ({
     [map, mapsData],
   );
 
-  useEffect(() => {
-    if (!map) {
-      initializeMap();
-    }
-    if (map && name) {
-      const yearStr = year || "general";
-      loadMapLayer(name, yearStr);
-      if (!isReduced) addPopupEffect(name, yearStr);
+  const cleanOcaLayers = (map: maplibregl.Map) => {
+    const mapLayers: string[] = map
+      ?.getLayersOrder()
+      .filter((layer: string) => layer.startsWith("@oca/"));
+    mapLayers.forEach((layer: string) => {
+      if (map?.getLayer(layer)) {
+        map?.removeLayer(layer);
+      }
+    });
+  };
 
-      return () => {
-        if (map?.getLayer(name + yearStr)) {
-          map?.removeLayer(name + yearStr);
-        }
-      };
-    }
-  }, [
-    name,
-    year,
-    map,
-    mapsData,
-    isReduced,
-    setLoading,
-    initializeMap,
-    loadMapLayer,
-    addPopupEffect,
-  ]);
+  useEffect(
+    () => {
+      if (map && name) {
+        const yearStr = year || "general";
+        if (!isReduced) addPopupEffect(name, yearStr);
+        loadMapLayer(name, yearStr);
+
+        return () => {
+          cleanOcaLayers(map);
+        };
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [name, year],
+  );
+
+  useEffect(
+    () => {
+      if (!map) {
+        initializeMap();
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [map, mapsData, isReduced, initializeMap],
+  );
 
   return (
     <MapContainer
