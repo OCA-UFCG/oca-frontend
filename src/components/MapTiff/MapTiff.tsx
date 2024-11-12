60363f44b2310bdf91549661eb303728643c0d22
cilasmarques <cilas@copin.ufcg.edu.br>
2024-11-11 23:13:59 -0300

    fix: layers loading

--- src/components/MapTiff/MapTiff.tsx
+++ src/components/MapTiff/MapTiff.tsx
@@ -12,8 +12,7 @@ import {
 } from "@/utils/constants";
 import MapPopup from "../MapPopup/MapPopup";
 import { MapTiffContext } from "@/contexts/MapContext";
-
-const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
+import { fetchMapSources } from "@/services/mapServices";
 
 const MapTiff = ({ isReduced = false, ...props }: { isReduced?: boolean }) => {
   const {
@@ -25,6 +24,7 @@ const MapTiff = ({ isReduced = false, ...props }: { isReduced?: boolean }) => {
     setDescRetracted,
   } = useContext(MapTiffContext);
 
+  const [loadedSources, setLoadedSources] = useState(new Set<string>());
   const { id, year } = currentVisu;
   const [map, setMap] = useState<maplibregl.Map | null>(null);
   const mapContainer = useRef<HTMLDivElement>(null);
@@ -32,141 +32,131 @@ const MapTiff = ({ isReduced = false, ...props }: { isReduced?: boolean }) => {
     new maplibregl.Popup({ closeButton: false, closeOnClick: false }),
   );
 
-  const initializeMap = useCallback(() => {
-    if (mapContainer.current) {
-      const newMap = new maplibregl.Map({
-        container: mapContainer.current!,
-        style: MAP_TIFF_STYLE,
-        center: isReduced ? [-54.69, -15.13] : [-51.55, -15],
-        zoom: isReduced ? 3 : 3.6,
-        minZoom: isReduced ? 3 : 3.6,
-        maxZoom: 10,
-      });
+  const showOcaLayer = (map: maplibregl.Map, sourceKey: string) => {
+    const symbolLayer = map
+      .getStyle()
+      ?.layers?.find((layer) => layer.type === "symbol");
 
-      newMap.on("load", () => {
-        if (!isReduced) {
-          newMap.addControl(new maplibregl.NavigationControl(), "bottom-left");
-        }
-
-        newMap.addSource("brazil-states", {
-          type: "geojson",
-          data: MAP_TIFF_BRAZIL_STATES,
-        });
-
-        newMap.addLayer({
-          id: "state-fills",
-          type: "fill",
-          source: "brazil-states",
-          layout: {},
-          paint: {
-            "fill-color": "black",
-            "fill-opacity": [
-              "case",
-              ["boolean", ["feature-state", "hover"], false],
-              0.3,
-              0,
-            ],
-          },
-        });
-
-        newMap.addLayer({
-          id: "state-borders",
-          type: "line",
-          source: "brazil-states",
-          layout: {},
-          paint: {
-            "line-color": "#2D2D2D",
-            "line-width": [
-              "case",
-              ["boolean", ["feature-state", "hover"], false],
-              2.5,
-              1.5,
-            ],
-          },
-        });
-
-        newMap.addSource("brazil-cities", {
-          type: "geojson",
-          data: MAP_TIFF_BRAZIL_CITIES,
-        });
-
-        newMap.addLayer({
-          id: "brazil-cities",
-          type: "line",
-          source: "brazil-cities",
-          layout: {},
-          paint: {
-            "line-color": "#00000050",
-            "line-width": 2,
-          },
-          minzoom: 6,
-        });
-      });
-
-      setMap(newMap);
+    if (map.getSource(sourceKey) && symbolLayer) {
+      map.addLayer(
+        {
+          type: "raster",
+          source: sourceKey,
+          id: sourceKey,
+        },
+        symbolLayer?.id,
+      );
     }
-  }, [isReduced, mapContainer]);
+  };
 
-  const loadSource = useCallback(
-    async (id: string, year: string) => {
-      if (!map?.getSource(id + year) && tiffs.length > 0) {
-        const mapData = tiffs.find((data) => data.fields.id === id)?.fields;
-        const body = JSON.stringify(mapData);
+  const cleanOcaLayers = (map: maplibregl.Map) => {
+    const mapLayers: string[] = map
+      ?.getLayersOrder()
+      .filter((layer: string) => layer.startsWith("@oca/"));
+    mapLayers.forEach((layer: string) => {
+      if (map?.getLayer(layer)) {
+        map?.removeLayer(layer);
+      }
+    });
+  };
 
-        // Fetch the raster layer from the ee API
-        const response = await fetch(
-          `${HOST_URL}/api/ee?name=${id}&year=${year}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body,
-          },
-        );
-
-        if (response.status !== 200) {
-          setLoading(false);
-
-          return;
-        }
-
-        const { url } = await response.json();
-        if (!map?.getSource(id + year)) {
-          map?.addSource(id + year, {
+  const loadSources = useCallback(
+    async (map: maplibregl.Map) => {
+      // === Add current raster source
+      const yearStr = year ? year : "general";
+      const mapData = tiffs.find((data) => data.fields.id === id)?.fields;
+      fetchMapSources(id, yearStr, JSON.stringify(mapData))
+        .then(({ url }) => {
+          map.addSource(`@oca/${id}${yearStr}`, {
             type: "raster",
             tiles: [url],
             tileSize: isReduced ? 64 : 128,
           });
-        }
-      }
+        })
+        .then(() => {
+          for (const additionalMapData of tiffs) {
+            for (const y of Object.keys(additionalMapData.fields.imageData)) {
+              const cId = additionalMapData.fields.id;
+              const sourceKey = `@oca/${cId}${y}`;
+
+              if (map.getSource(sourceKey)) continue;
+              if (cId === id && y === yearStr) continue;
+
+              fetchMapSources(cId, y, JSON.stringify(additionalMapData.fields))
+                .then(({ url: additionalUrl }) => {
+                  map.addSource(sourceKey, {
+                    type: "raster",
+                    tiles: [additionalUrl],
+                    tileSize: isReduced ? 64 : 128,
+                  });
+                })
+                .catch((error) => {
+                  console.error("Erro ao carregar fontes de mapa.", error);
+                });
+            }
+          }
+        })
+        .catch((error) => {
+          console.error("Erro ao carregar fontes de mapa.", error);
+        });
+
+      // === Add Brazil states source
+      map.addSource("brazil-states", {
+        type: "geojson",
+        data: MAP_TIFF_BRAZIL_STATES,
+      });
+
+      map.addLayer({
+        id: "state-fills",
+        type: "fill",
+        source: "brazil-states",
+        layout: {},
+        paint: {
+          "fill-color": "black",
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            0.3,
+            0,
+          ],
+        },
+      });
+
+      map.addLayer({
+        id: "state-borders",
+        type: "line",
+        source: "brazil-states",
+        layout: {},
+        paint: {
+          "line-color": "#2D2D2D",
+          "line-width": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            2.5,
+            1.5,
+          ],
+        },
+      });
+
+      // === Add Brazil cities source
+      map.addSource("brazil-cities", {
+        type: "geojson",
+        data: MAP_TIFF_BRAZIL_CITIES,
+      });
+
+      map.addLayer({
+        id: "brazil-cities",
+        type: "line",
+        source: "brazil-cities",
+        layout: {},
+        paint: {
+          "line-color": "#00000050",
+          "line-width": 2,
+        },
+        minzoom: 6,
+      });
     },
-    [map, tiffs, setLoading, isReduced],
-  );
-
-  const loadMapLayer = useCallback(
-    async (id: string, year: string) => {
-      setLoading(true);
-      await loadSource(id, year);
-
-      const symbolLayer = map
-        ?.getStyle()
-        ?.layers?.find((layer) => layer.type === "symbol");
-
-      if (map && symbolLayer) {
-        map.addLayer(
-          {
-            type: "raster",
-            source: `${id}${year}`,
-            id: `@oca/${id}${year}`,
-          },
-          symbolLayer.id,
-        );
-      }
-
-      setLoading(false);
-    },
-    [map, setLoading, loadSource],
+    [id, year, tiffs, isReduced],
   );
 
   const addPopupEffect = useCallback(
@@ -232,29 +222,54 @@ const MapTiff = ({ isReduced = false, ...props }: { isReduced?: boolean }) => {
     [map, tiffs],
   );
 
-  const cleanOcaLayers = (map: maplibregl.Map) => {
-    const mapLayers: string[] = map
-      ?.getLayersOrder()
-      .filter((layer: string) => layer.startsWith("@oca/"));
-    mapLayers.forEach((layer: string) => {
-      if (map?.getLayer(layer)) {
-        map?.removeLayer(layer);
-      }
-    });
-  };
+  const initializeMap = useCallback(() => {
+    if (mapContainer.current) {
+      const newMap = new maplibregl.Map({
+        container: mapContainer.current!,
+        style: MAP_TIFF_STYLE,
+        center: isReduced ? [-54.69, -15.13] : [-51.55, -15],
+        zoom: isReduced ? 3 : 3.6,
+        minZoom: isReduced ? 3 : 3.6,
+        maxZoom: 10,
+      });
+
+      newMap.on("load", async () => {
+        if (!isReduced)
+          newMap.addControl(new maplibregl.NavigationControl(), "bottom-left");
+
+        loadSources(newMap);
+      });
+
+      newMap.on("sourcedata", (e) => {
+        if (e.sourceId && e.isSourceLoaded && !loadedSources.has(e.sourceId)) {
+          setLoadedSources((prev) => {
+            const newSet = new Set(prev.add(e.sourceId));
+            newSet.add(e.sourceId);
+
+            return newSet;
+          });
+        }
+      });
+
+      setMap(newMap);
+    }
+  }, [isReduced, mapContainer, loadedSources, loadSources]);
 
   useEffect(() => {
-    if (map && id && year) {
-      if (!isReduced) addPopupEffect(id, year);
-      loadMapLayer(id, year);
+    const yearStr = year ? year : "general";
+    const sourceKey = `@oca/${id}${yearStr}`;
 
-      return () => {
+    if (loadedSources.has(sourceKey)) {
+      if (map && !map.getLayer(sourceKey)) {
+        setLoading(true);
         cleanOcaLayers(map);
-      };
+        addPopupEffect(id, yearStr);
+        showOcaLayer(map, sourceKey);
+        setLoading(false);
+      }
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id, year, map]);
+  }, [id, year, loadedSources]);
 
   useEffect(() => {
     if (!map) {
